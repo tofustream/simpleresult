7d9c1176e6b6bde3b42d230447552f1390d65d7c
tofustream <177463621+tofustream@users.noreply.github.com>
2024-12-03 23:06:58 +0900

    Update result.py

--- simpleresult/result.py
+++ simpleresult/result.py
@@ -1,39 +1,41 @@
 from dataclasses import dataclass, field
-import typing as t
+from typing import Generic, TypeVar, Optional
 
 from .error import Error
 
+T = TypeVar("T")
+
 
 @dataclass(frozen=True)
-class Result:
+class Result(Generic[T]):
     """
     A class representing the result of an operation, which can be either a success or a failure.
 
     Attributes:
-        __value (t.Any): The value of a successful result.
-        __error (t.Optional[Error]): The error message of a failed result.
+        _value (Optional[T]): The value of a successful result.
+        _error (Optional[Error]): The error message of a failed result.
     """
 
-    __value: t.Any = field(default=None, init=False)
-    __error: t.Optional[Error] = field(default=None, init=False)
+    _value: Optional[T] = field(default=None, init=False)
+    _error: Optional[Error] = field(default=None, init=False)
 
     @staticmethod
-    def success(value: t.Any) -> "Result":
+    def success(value: T) -> "Result[T]":
         """
         Creates a Result object representing a successful operation.
 
         Args:
-            value (t.Any): The value of the successful result.
+            value (T): The value of the successful result.
 
         Returns:
-            Result: A Result object containing the success value.
+            Result[T]: A Result object containing the success value.
         """
-        result = Result()
-        object.__setattr__(result, "_Result__value", value)
+        result = Result[T]()
+        object.__setattr__(result, "_value", value)
         return result
 
     @staticmethod
-    def failure(message: str) -> "Result":
+    def failure(message: str) -> "Result[T]":
         """
         Creates a Result object representing a failed operation.
 
@@ -41,10 +43,10 @@ class Result:
             message (str): The error message of the failed result.
 
         Returns:
-            Result: A Result object containing the error message.
+            Result[T]: A Result object containing the error message.
         """
-        result = Result()
-        object.__setattr__(result, "_Result__error", Error(message))
+        result = Result[T]()
+        object.__setattr__(result, "_error", Error(message))
         return result
 
     def has_error(self) -> bool:
@@ -54,24 +56,24 @@ class Result:
         Returns:
             bool: True if the Result object contains an error, False otherwise.
         """
-        return self.__error is not None
+        return self._error is not None
 
     @property
-    def value(self) -> t.Any:
+    def value(self) -> Optional[T]:
         """
         Gets the value of a successful result.
 
         Returns:
-            t.Any: The value of the successful result.
+            Optional[T]: The value of the successful result.
         """
-        return self.__value
+        return self._value
 
     @property
-    def error(self) -> t.Optional[Error]:
+    def error(self) -> Optional[Error]:
         """
         Gets the error message of a failed result.
 
         Returns:
-            t.Optional[Error]: The error message of the failed result.
+            Optional[Error]: The error message of the failed result.
         """
-        return self.__error
+        return self._error
